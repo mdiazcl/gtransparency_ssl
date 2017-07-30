668ca9e363e4b41053d0514e35673d3c4c96aa74
Mdiazcl <mdiazl@entel.cl>
2017-07-30 18:18:28 -0400

    test data

--- test.py
+++ test.py
@@ -1,8 +1,8 @@
 from fetcher import fetchData
 import time
 
-certificados, entidades = fetchData("cge.cl", include_subdomains=True, include_expired=True)
+certificados, entidades = fetchData("mdiazlira.com", include_subdomains=True, include_expired=True)
 for certificado in certificados:
 	fr = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(certificado.valid_from/1000))
 	to = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(certificado.valid_to/1000))
-	print "{0} \t|{1}\t|{2}".format(certificado.subject, fr, to)
+	print "- {0}\t| {3}\t| {1}\t| {2}\t".format(certificado.subject, fr, to, certificado.issuer)
